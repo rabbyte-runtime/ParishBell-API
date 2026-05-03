0ef327081e5d953281b092b132b277b5c96b5055
Rabbyte 🐰 <rajitha_dassanayake@icloud.com>
2026-05-03 23:52:51 +0530

    chore: add API controllers and map

--- ParishBell.API/Program.cs
+++ ParishBell.API/Program.cs
@@ -1,14 +1,20 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using ParishBell.Application.Services;
 using ParishBell.Core.Interfaces;
 using ParishBell.Infrastructure.BackgroundJobs;
 using ParishBell.Infrastructure.Caching;
 using ParishBell.Infrastructure.Data;
 using ParishBell.Infrastructure.Repositories;
+using ParishBell.Infrastructure.Security;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // NOTE: Add OpenApi support
 builder.Services.AddOpenApi();
+
+// NOTE: Add controllers
+builder.Services.AddControllers();
 
 // NOTE: Register the DbContext
 builder.Services.AddDbContext<ParishBellDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -24,6 +30,12 @@ builder.Services.AddScoped<IMessageCache, MessageCache>();
 builder.Services.AddHostedService<MessageCacheStartupService>();
 builder.Services.AddHostedService<MessageCacheRefreshJob>();
 
+// NOTE: Service registrations
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
+
 var app = builder.Build();
 
 // NOTE: Configure the HTTP request pipeline.
@@ -33,5 +45,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+// NOTE: Redirect HTTP requests to HTTPS
 app.UseHttpsRedirection();
+
+// NOTE: Map controller endpoints
+app.MapControllers();
+
 app.Run();

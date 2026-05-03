c0817f3d29bd53d1144721a22ae6eac471b2742a
Rabbyte 🐰 <rajitha_dassanayake@icloud.com>
2026-05-03 23:43:28 +0530

    fix(message-caching): register ParishbellDbContext in Program.cs to allow caching messages

--- ParishBell.API/Program.cs
+++ ParishBell.API/Program.cs
@@ -1,12 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using ParishBell.Core.Interfaces;
 using ParishBell.Infrastructure.BackgroundJobs;
 using ParishBell.Infrastructure.Caching;
+using ParishBell.Infrastructure.Data;
 using ParishBell.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // NOTE: Add OpenApi support
 builder.Services.AddOpenApi();
+
+// NOTE: Register the DbContext
+builder.Services.AddDbContext<ParishBellDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // NOTE: Add Memory Cache
 builder.Services.AddMemoryCache();
